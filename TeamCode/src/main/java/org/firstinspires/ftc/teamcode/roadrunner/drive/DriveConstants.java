08253722e3b4d4291749587c2fac71913a1bdeee
rjan <101954736+rjan939@users.noreply.github.com>
2024-01-23 21:04:21 -0600

    Fixed DriveConstants with actual gear ratios(cause ultraplanetary motors are weird)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -21,8 +21,8 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 28;
-    public static final double MAX_RPM = 6000;
+    public static final double TICKS_PER_REV = 1054.557532;
+    public static final double MAX_RPM = 159.308520306;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -45,7 +45,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8898; // in
-    public static double GEAR_RATIO = (1 / 48.0); // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = (1 / 37.662769); // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 14; // in
 
     /*
